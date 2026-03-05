b06d53991fcf6a2247235da0fb9c1fdc4816d7e6
Ivan Vostrecov <ivan.betep@gmail.com>
2026-03-05 15:29:08 +0300

    Добавлен декоратор login_required

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm
 
@@ -54,6 +55,7 @@ def user_login_view(request):
 #     return render(request, 'users/user_profile_read_only.html', context)
 """
 
+@login_required(login_url='users:user_login')
 def user_profile_view(request):
     user_object = request.user
     context = {
@@ -61,6 +63,7 @@ def user_profile_view(request):
     }
     return render(request, 'users/user_profile_read_only.html', context=context)
 
+@login_required(login_url='users:user_login')
 def user_update_view(request):
     user_object = request.user
     if request.method == 'POST':
